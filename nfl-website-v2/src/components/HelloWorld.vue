d09ade1f0be85474dce62e6dbb5bc7f7f1048601
Jeff Cairns <jeff12ca@gmail.com>
2024-12-10 13:58:29 -0600

    skeleton

---
+++ nfl-website-v2/src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+</script>
+
+<template>
+  <div class="hello">
+    <p>Website</p>
+</div>
+</template>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
